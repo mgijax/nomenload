eaa20ed5c9e59e233b9cc5ee31d16b1944702841
MGI Admin <mgiadmin@jax.org>
2015-10-02 18:43:09 +0000

    TR12070/11216/nomen

--- bin/nomenload.sh
+++ bin/nomenload.sh
@@ -178,12 +178,16 @@ fi
 #
 # set permissions
 #
-chmod -f 775 ${FILEDIR}/*
-chgrp mgi ${FILEDIR}/*
-chgrp mgi ${FILEDIR}/*/*
-chmod -f 775 ${DESTFILEDIR}/*
-chgrp mgi ${DESTFILEDIR}/*
-chgrp mgi ${DESTFILEDIR}/*/*
+#case `whoami` in
+#    mgiadmin)
+#	chmod -f 775 ${FILEDIR}/*
+#	chgrp -f mgi ${FILEDIR}/*
+#	chgrp -f mgi ${FILEDIR}/*/*
+#	chmod -f 775 ${DESTFILEDIR}/*
+#	chgrp -f mgi ${DESTFILEDIR}/*
+#	chgrp -f mgi ${DESTFILEDIR}/*/*
+#	;;
+#esac
 
 #
 # Archive : publshed only
