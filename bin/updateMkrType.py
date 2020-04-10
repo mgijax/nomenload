691ccc126bb14515b439af2c4130ffe30243ac2a
Lori Corbani <lori.corbani@jax.org>
2020-04-10 12:55:14 -0400

    TR13204/Infrastructure/python 3

--- bin/updateMkrType.py
+++ bin/updateMkrType.py
@@ -1,12 +1,9 @@
-#!/usr/local/bin/python
-
 # update a set of markers to marker type 
 # update modification date
 # update modified by 
 
 import sys
 import os
-import string
 import db
 import mgi_utils
 import accessionlib
@@ -51,34 +48,34 @@ modifiedByKey = results[0]['_User_key']
 #
 # Create mouse marker MGI ID to marker key lookup
 results = db.sql('''select a.accid, m._Marker_key
-	from ACC_Accession a, MRK_Marker m
-	where a._MGIType_key = 2
-	and a._LogicalDB_key = 1
-	and a.preferred = 1
-	and a.prefixPart = 'MGI:'
-	and a._Object_key = m._Marker_key
-	and m._Organism_key = 1''', 'auto')
+        from ACC_Accession a, MRK_Marker m
+        where a._MGIType_key = 2
+        and a._LogicalDB_key = 1
+        and a.preferred = 1
+        and a.prefixPart = 'MGI:'
+        and a._Object_key = m._Marker_key
+        and m._Organism_key = 1''', 'auto')
 for r in results:
     mgiToMrkKeyDict[r['accid']] = r['_Marker_key']
 
 # iterate thru the file creating list of marker keys to update
 for line in inputFile.readlines():
-    tokens = string.split(line[:-1], '\t')
-    mgiID = string.strip(tokens[0])
+    tokens = str.split(line[:-1], '\t')
+    mgiID = str.strip(tokens[0])
 
     if mgiID not in mgiToMrkKeyDict:
-	print '%s is not a valid mouse ID' % mgiID
-	exit (1, 'Invalid mouse ID %s\n' % mgiID)
+        print('%s is not a valid mouse ID' % mgiID)
+        exit (1, 'Invalid mouse ID %s\n' % mgiID)
     updateList.append(mgiToMrkKeyDict[mgiID])
 
 for key in updateList:
     cmd = '''
-	update MRK_Marker
-	set _Marker_Type_key = %s, 
-	modification_date = '%s', 
+        update MRK_Marker
+        set _Marker_Type_key = %s, 
+        modification_date = '%s', 
         _ModifiedBy_key = %s 
-	where _Marker_key = %s''' % (newMkrTypeKey, loaddate, modifiedByKey, key)
-    print cmd
+        where _Marker_key = %s''' % (newMkrTypeKey, loaddate, modifiedByKey, key)
+    print(cmd)
     db.sql(cmd, 'auto')
 
 db.commit()
