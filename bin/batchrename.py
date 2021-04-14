d450be8af0bad229765ad631527dd5238cb3f331
Lori Corbani <lori.corbani@jax.org>
2021-04-14 09:46:48 -0400

    WTS2-480/added Batch Rename

---
+++ bin/batchrename.py
@@ -0,0 +1,295 @@
+'''
+#
+# Purpose:
+#
+#	1) To rename a list of existing Markers
+#	    MRK_Marker
+#	    MGI_History
+#
+# Input:
+#
+#	A tab-delimited file in the format:
+#               field 1: MGI:xxxx
+#               field 2: Current Symbol Name
+#               field 3: New Symbol Name
+#               field 4: J:xxx
+#               field 5: Reason for Rename : use *exact* term as in the list of Reasons in the PWI/Marker module
+#               field 6: Add Current As Synonym : "y" or "no"
+#               field 7: User : yz
+#
+# Parameters:
+#
+#	Diagnostics file of all input parameters and SQL commands
+#	Error file
+#
+# lec	09/22/2020
+#	- TR13233/add batch rename script
+#
+'''
+
+import sys
+import os
+import db
+import mgi_utils
+import loadlib
+
+#db.setTrace()
+
+DEBUG = 0
+
+#
+# from configuration file
+#
+mode = os.environ['NOMENMODE']
+inputFileName = os.environ['RENAME_FILE_DEFAULT']
+diagFileName = os.environ['RENAME_LOG_DIAG']
+errorFileName = os.environ['RENAME_LOG_ERROR']
+
+inputFile = ''		# file descriptor
+diagFile = ''		# file descriptor
+errorFile = ''		# file descriptor
+lineNum = 0
+
+eventReasonLookup = {}
+
+markerID = ''
+symbol = ''
+name = ''
+jnum = ''
+eventReason = ''
+addAsSynonym = 0
+createdBy = ''
+markerKey = ''
+refKey = ''
+eventReasonKey = ''
+createdByKey = ''
+
+def exit(status, message = None):
+    '''
+    # requires: status, the numeric exit status (integer)
+    #           message (string)
+    #
+    # effects:
+    # Print message to stderr and exits
+    #
+    # returns:
+    #
+    '''
+
+    db.commit()
+    db.useOneConnection()
+
+    if message is not None:
+        sys.stderr.write('\n' + str(message) + '\n')
+
+    try:
+        inputFile.close()
+        diagFile.flush()
+        errorFile.flush()
+        diagFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
+        errorFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
+        diagFile.close()
+        errorFile.close()
+    except:
+        pass
+
+    sys.exit(status)
+ 
+def init():
+    '''
+    # requires: 
+    #
+    # effects: 
+    # 1. Processes command line options
+    # 2. Initializes local DBMS parameters
+    # 3. Initializes global file descriptors/file names
+    #
+    # returns:
+    #
+    '''
+
+    global inputFile, diagFile, diagFileName
+    global erroFile, errorFileName
+    global eventReasonLookup
+
+    db.useOneConnection(1)
+
+    try:
+        inputFile = open(inputFileName, 'r')
+    except:
+        exit(1, 'Could not open file %s\n' % inputFileName)
+            
+    try:
+        diagFile = open(diagFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % diagFileName)
+            
+    try:
+        errorFile = open(errorFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % errorFileName)
+            
+    results = db.sql('select * from MRK_EventReason', 'auto')
+    for r in results:
+        key = r['eventreason']
+        value = r['_marker_eventreason_key']
+        eventReasonLookup[key] = []
+        eventReasonLookup[key].append(value)
+    #print(eventReasonLookup)
+
+    # Log all SQL 
+    db.set_sqlLogFunction(db.sqlLogAll)
+
+    # Set Log File Descriptor
+    db.set_commandLogFile(diagFileName)
+
+    # Set Log File Descriptor
+    diagFile.write('Start Date/Time: %s\n' % (mgi_utils.date()))
+    diagFile.write('Server: %s\n' % (db.get_sqlServer()))
+    diagFile.write('Database: %s\n' % (db.get_sqlDatabase()))
+    diagFile.write('Input File: %s\n' % (inputFileName))
+
+def verifyMode():
+    '''
+    # requires:
+    #
+    # effects:
+    #	Verifies the processing mode is valid.  If it is not valid,
+    #	the program is aborted.
+    #	Sets globals based on processing mode.
+    #	Deletes data based on processing mode.
+    #
+    # returns:
+    #	nothing
+    #
+    '''
+
+    global DEBUG
+
+    if mode == 'preview':
+        DEBUG = 1
+    elif mode not in ['load']:
+        exit(1, 'Invalid Processing Mode:  %s\n' % (mode))
+
+def sanityCheck():
+    '''
+    #
+    # requires:
+    #
+    # effects:
+    #
+    # returns:
+    #	0 if sanity check passes
+    #	1 if sanity check fails
+    #
+    '''
+
+    global markerKey
+    global refKey
+    global eventReasonKey
+    global createdByKey
+    global name
+
+    error = 0
+
+    markerKey = loadlib.verifyMarker(markerID, lineNum, errorFile)
+    if markerKey > 0:
+        results = db.sql(''' select name from MRK_Marker where _marker_key = %s ''' % (markerKey), 'auto')
+        name = results[0]['name']
+
+    refKey = loadlib.verifyReference(jnum, lineNum, errorFile)
+    createdByKey = loadlib.verifyUser(createdBy, lineNum, errorFile)
+
+    if eventReason not in eventReasonLookup:
+        eventReasonKey = 0
+    else:
+        eventReasonKey = eventReasonLookup[eventReason][0]
+
+    if markerKey == 0 or \
+       refKey == 0 or \
+       eventReasonKey == 0 or \
+       createdByKey == 0:
+
+        # set error flag to true
+        error = 1
+
+    return (error)
+
+def processFile():
+    '''
+    # requires:
+    #
+    # effects:
+    #	Reads input file
+    #	Verifies and Processes each line in the input file
+    #
+    # returns:
+    #	nothing
+    #
+    '''
+
+    global lineNum
+    global markerID
+    global symbol 
+    global name
+    global jnum
+    global eventReason
+    global addAsSynonym
+    global createdBy
+
+    # For each line in the input file
+
+    for line in inputFile.readlines():
+
+        lineNum = lineNum + 1
+
+        # Split the line into tokens
+        tokens = str.split(line[:-1], '\t')
+
+        try:
+            markerID = tokens[0]
+            symbol = tokens[2].replace("'", "''")
+            name = tokens[3].replace("'", "''")
+            jnum = tokens[4]
+            eventReason = tokens[5]
+            createdBy = tokens[7]
+
+            if tokens[6] == 'y':
+                addAsSynonym = 1
+            else:
+                addAsSynonym = 0
+
+        except:
+            errorFile.write('Invalid Line (missing column(s)) (row %d): %s\n' % (lineNum, line))
+            continue
+
+        #
+        # sanity checks
+        #
+
+        if sanityCheck() == 1:
+            errorFile.write(str(tokens) + '\n\n')
+            continue
+
+        cmd = '''select * from MRK_simpleWithdrawal(%s,%s,%s,%s,'%s','%s',%d);\n''' \
+                % (createdByKey, markerKey, refKey, eventReasonKey, symbol, name, addAsSynonym)
+        diagFile.write(cmd)
+
+        if not DEBUG:
+                db.sql(cmd, None)
+                db.commit()
+
+    # end of "for line in inputFile.readlines():"
+
+#
+# Main
+#
+
+#print 'init()'
+init()
+
+#print 'verifyMode()'
+verifyMode()
+
+#print 'processFile()'
+processFile()
--- bin/batchrename.sh
+++ bin/batchrename.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 #
-#  nomenload.sh
+#  batchrename.sh
 ###########################################################################
 #
 #  Purpose:
-# 	This script runs the Nomenclature & Mapping load
+# 	This script runs the Nomenclature Batch Rename load
 #
-  Usage=nomenload.sh
+  Usage=batchrename.sh
 #
 #  Env Vars:
 #
-#      See the configuration file nomenload.config
+#      See the configuration file batchrename.config
 #
 #  Inputs:
 #
-#      - configuration file - nomenload.config
-#      - input file - see nomenload.config
+#      - configuration file - batchrename.config
+#      - input file - see batchrename.config
 #
 #  Outputs:
 #
 #      - An archive file
-#      - Log files defined by the environment variables ${LOG_PROC},
-#        ${LOG_FILE}, ${LOG_FILE_CUR}, ${LOG_FILE_VAL}, ${LOG_ERROR}
-#      - nomenload logs and bcp file to ${OUTPUTDIR}
+#      - Log files defined by the environment variables 
+#      - batchrename logs and bcp file to ${OUTPUTDIR}
 #      - mappingload logs and bcp files  - see mappingload
 #      - Records written to the database tables
 #      - Exceptions written to standard error
@@ -42,15 +41,15 @@
 #      4) Initialize the log file.
 #      5) Determine if the input file has changed since the last time that
 #         the load was run. Do not continue if the input file is not new.
-#      6) Load nomenload using configuration file
+#      6) Load batchrename using configuration file
 #      7) Load mappingload using configuration file
 #      8) Archive the input file.
-#      9) Touch the "lastrun" file to timestamp the last run of the load.
+#      9) Touch the "lastrun.batchrename" file to timestamp the last run of the load.
 #
 # History:
 #
 # lec	09/28/2015
-#	- new (using nomenload as a template)
+#	- new (using batchrename as a template)
 #
 
 cd `dirname $0`
@@ -61,10 +60,19 @@ cd `dirname $0`
 CONFIG_FILE=$1
 . ${CONFIG_FILE}
 
-MAIL_LOADNAME="${NOMENMODE} : Nomen/Mapping Load"
+MAIL_LOADNAME="${NOMENMODE} : Batch Rename Load"
 export MAIL_LOADNAME
 
-rm -rf ${LOG_FILE} ${LOG_PROC} ${LOG_DIAG} ${LOG_CUR} ${LOG_VAL} ${LOG_ERROR}
+# change names to ${LOG}
+LOG_FILE=${RENAME_LOG_FILE} 
+LOG_PROC=${RENAME_LOG_PROC} 
+LOG_DIAG=${RENAME_LOG_DIAG} 
+LOG_CUR=${RENAME_LOG_CUR} 
+LOG_VAL=${RENAME_LOG_VAL} 
+LOG_ERROR=${RENAME_LOG_ERROR}
+export LOG_FILE LOG_PROC LOG_DIAG LOG_CUR LOG_VAL LOG_ERROR
+
+rm -rf ${RENAME_LOG_FILE} ${RENAME_LOG_PROC} ${RENAME_LOG_DIAG} ${RENAME_LOG_CUR} ${RENAME_LOG_VAL} ${RENAME_LOG_ERROR}
 
 #
 # use user-provied value or use config/default value
@@ -72,11 +80,11 @@ rm -rf ${LOG_FILE} ${LOG_PROC} ${LOG_DIAG} ${LOG_CUR} ${LOG_VAL} ${LOG_ERROR}
 #
 if [ $# -eq 2 ] 
 then
-    INPUT_FILE_DEFAULT=$2
+    RENAME_FILE_DEFAULT=$2
 fi
-if [ ! -r ${INPUT_FILE_DEFAULT} ]
+if [ ! -r ${RENAME_FILE_DEFAULT} ]
 then
-    echo "Missing input file: ${INPUT_FILE_DEFAULT}" | tee -a ${LOG_FILE}
+    echo "Missing input file: ${RENAME_FILE_DEFAULT}" | tee -a ${RENAME_LOG_FILE}
     exit 1
 fi
 
@@ -90,11 +98,11 @@ then
     then
         . ${DLAJOBSTREAMFUNC}
     else
-        echo "Cannot source DLA functions script: ${DLAJOBSTREAMFUNC}" | tee -a ${LOG_FILE}
+        echo "Cannot source DLA functions script: ${DLAJOBSTREAMFUNC}" | tee -a ${RENAME_LOG_FILE}
         exit 1
     fi  
 else
-    echo "Environment variable DLAJOBSTREAMFUNC has not been defined." | tee -a ${LOG_FILE}
+    echo "Environment variable DLAJOBSTREAMFUNC has not been defined." | tee -a ${RENAME_LOG_FILE}
     exit 1
 fi
 
@@ -109,25 +117,25 @@ fi
 #
 if [ ${NOMENMODE} != "preview" ]
 then
-    startLog ${LOG_PROC} ${LOG_DIAG} ${LOG_CUR} ${LOG_VAL} | tee -a ${LOG}
-    getConfigEnv >> ${LOG_PROC}
-    getConfigEnv -e >> ${LOG_DIAG}
+    startLog ${RENAME_LOG_PROC} ${RENAME_LOG_DIAG} ${RENAME_LOG_CUR} ${RENAME_LOG_VAL} | tee -a ${RENAME_LOG}
+    getConfigEnv >> ${RENAME_LOG_PROC}
+    getConfigEnv -e >> ${RENAME_LOG_DIAG}
 fi
 
 #
-# There should be a "lastrun" file in the input directory that was created
+# There should be a "lastrun.batchrename" file in the input directory that was created
 # the last time the load was run for this input file. If this file exists
 # and is more recent than the input file, the load does not need to be run.
 #
 if [ ${NOMENMODE} != "preview" ]
 then
-    LASTRUN_FILE=${INPUTDIR}/lastrun
+    LASTRUN_FILE=${INPUTDIR}/lastrun.batchrename
 
     if [ -f ${LASTRUN_FILE} ]
     then
-        if test ${LASTRUN_FILE} -nt ${INPUT_FILE_DEFAULT}
+        if test ${LASTRUN_FILE} -nt ${RENAME_FILE_DEFAULT}
         then
-            echo "SKIPPED: ${NOMENMODE} : Input file has not been updated" | tee -a ${LOG_FILE_PROC}
+            echo "SKIPPED: ${NOMENMODE} : Input file has not been updated" | tee -a ${RENAME_LOG_FILE_PROC}
 	    exit 0
         fi
     fi
@@ -137,34 +145,18 @@ fi
 # Convert the input file into a QC-ready version that can be used to run
 # the sanity/QC reports against.
 #
-dos2unix ${INPUT_FILE_DEFAULT} ${INPUT_FILE_DEFAULT} 2>/dev/null
+dos2unix ${RENAME_FILE_DEFAULT} ${RENAME_FILE_DEFAULT} 2>/dev/null
 
 #
 # Execute nomen load
 #
-echo "" | tee -a ${LOG_FILE}
-date | tee -a ${LOG_FILE}
-echo "Running nomenload : ${NOMENMODE}" | tee -a ${LOG_FILE}
+echo "" | tee -a ${RENAME_LOG_FILE}
+date | tee -a ${RENAME_LOG_FILE}
+echo "Running batchrename : ${NOMENMODE}" | tee -a ${RENAME_LOG_FILE}
 cd ${OUTPUTDIR}
-${PYTHON} ${NOMENLOAD}/bin/nomenload.py | tee -a ${LOG_DIAG}
+${PYTHON} ${NOMENLOAD}/bin/batchrename.py | tee -a ${RENAME_LOG_DIAG}
 STAT=$?
 checkStatus ${STAT} "${NOMENLOAD} ${CONFIG_FILE} : ${NOMENMODE} :"
-
-#
-# Execute mapping load if not "preview" and nomenload was successful
-#
-if [ ${NOMENMODE} != "preview" ]
-then
-	if [[ ${STAT} == 0 ]]
-	then
-		cd ${OUTPUTDIR}
-		${MAPPINGLOAD}/mappingload.sh ${CONFIG_FILE}
-		STAT=$?
-		checkStatus ${STAT} "${MAPPINGLOAD} ${CONFIG_FILE} : ${MAPPINGMODE} :"
-	else
-		echo "FATAL ERROR: nomenload exit status = ${STAT} : ${NOMENMODE}" | tee -a ${LOG_FILE}
-	fi
-fi
 
 #
 # set permissions
@@ -178,7 +170,7 @@ case `whoami` in
 	chgrp -f mgi ${DESTFILEDIR}/*
 	chgrp -f mgi ${DESTFILEDIR}/*/*
 	chgrp -f mgi ${NOMENLOAD}/bin
-	chgrp -f mgi ${NOMENLOAD}/bin/nomenload.log
+	chgrp -f mgi ${NOMENLOAD}/bin/batchrename.log
 	;;
 esac
 
@@ -188,11 +180,11 @@ esac
 #
 if [ ${NOMENMODE} != "preview" ]
 then
-    createArchive ${ARCHIVEDIR} ${LOGDIR} ${INPUTDIR} ${OUTPUTDIR} | tee -a ${LOG}
+    createArchive ${ARCHIVEDIR} ${RENAME_LOGDIR} ${RENAMEDIR} ${OUTPUTDIR} | tee -a ${RENAME_LOG}
 fi 
 
 #
-# Touch the "lastrun" file to note when the load was run.
+# Touch the "lastrun.batchrename" file to note when the load was run.
 #
 if [ ${NOMENMODE} != "preview" ]
 then
@@ -202,10 +194,10 @@ fi
 #
 # cat the error file
 #
-cat ${LOG_ERROR}
+cat ${RENAME_LOG_ERROR}
 
-echo "" | tee -a ${LOG_FILE}
-date | tee -a ${LOG_FILE}
+echo "" | tee -a ${RENAME_LOG_FILE}
+date | tee -a ${RENAME_LOG_FILE}
 
 #
 # run postload cleanup and email logs
